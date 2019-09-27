4c20e1d5bf483ceb44d5a95ac74796f060a7a7d7
ayerest <irisiseve@gmail.com>
2019-09-27 15:51:17 -0700

    added a fetch function to the profile container to hookup with the tomatoes controller in the backend for data scraping

--- moviepal-client/src/App.js
+++ moviepal-client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import Signup from './components/SignUp'
-// import Profile from './containers/Profile'
+import Profile from './containers/Profile'
 // import Settings from './containers/Settings'
 // import MovieListsContainer from './containers/MovieListsContainer'
 // import SignUpFormContainer from './containers/SignUpFormContainer'
@@ -10,31 +10,31 @@ import './App.css';
 
 function App() {
 
-  function testfunction () {
-    fetch("http://localhost3000/genres", {
-      method: 'POST',
-      headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: "test name" 
-      })
-    })
-  }
+  // function testfunction () {
+  //   fetch("http://localhost3000/genres", {
+  //     method: 'POST',
+  //     headers: {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       name: "test name" 
+  //     })
+  //   })
+  // }
     
   return (
     <div className="">
       <header className="">
       </header>
       {/* <Signup /> */}
-      {/* <Profile /> */}
-      <button onClick = {testfunction} >add test data </button>
+      <Profile />
+      {/* <button onClick = {testfunction} >add test data </button> */}
       {/* <Settings />
       <SignUpFormContainer />
       <MovieListsContainer /> */}
     </div>
   );
-}
+    }
 
 export default App;
--- moviepal-client/src/containers/Profile.js
+++ moviepal-client/src/containers/Profile.js
@@ -6,8 +6,16 @@ import CurrentMoviesContainer from './CurrentMoviesContainer'
 class Profile extends Component {
     constructor() {
         super()
-
-
+    fetch('http://localhost:3000/tomatoes', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            genres: ["Horror", "Action"]
+        })
+    }).then(response => response.json())
+        .then(data => console.log(data))
         
     }
     render() {
